7317f86c68e8f27c1a09941cec23993328fdf67f
Patrick Veverka <patrick@veverka.net>
2020-02-09 15:12:16 -0500

    updating

--- dns/cloudflare.tf
+++ dns/cloudflare.tf
@@ -18,137 +18,144 @@ provider "cloudflare" {
 resource "cloudflare_zone" "veverka" {
     zone = "veverka.net"
 }
-resource "cloudflare_record" "lan" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "lan.veverka.net"
-  value   = "71.120.237.171"
-  type    = "A"
-  ttl     = 120
-}
 
+resource "cloudflare_record" "lan" {
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "lan"
+  value    = "71.120.237.171"
+  type     = "A"
+  ttl      = 120
+}
+resource "cloudflare_record" "terraform" {
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "terraform"
+  value    = "192.168.0.11"
+  type     = "A"
+  ttl      = 120
+}
 resource "cloudflare_record" "unifi" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "unifi.veverka.net"
-  value   = "192.168.1.6"
-  type    = "A"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "unifi"
+  value    = "192.168.1.6"
+  type     = "A"
+  ttl      = 120
 }
 resource "cloudflare_record" "veverkanet" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "veverka.net"
-  value   = "71.120.237.171"
-  type    = "A"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "veverkanet"
+  value    = "71.120.237.171"
+  type     = "A"
+  ttl      = 120
 }
 resource "cloudflare_record" "windows" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "windows.veverka.net"
-  value   = "192.168.1.43"
-  type    = "A"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "windows"
+  value    = "192.168.1.43"
+  type     = "A"
+  ttl      = 120
 }
 resource "cloudflare_record" "bazarr" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "bazarr.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "bazarr"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "consul" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "consul.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "consul"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "couchpotato" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "couchpotato.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "couchpotato"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "emailmg" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "email.mg.veverka.net"
-  value   = "mailgun.org"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "emailmg"
+  value    = "mailgun.org"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "homeassistant" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "homeassistant.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "homeassistant"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "jackett" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "jackett.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "jackett"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "nomad" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "nomad.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "nomad"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "ombi" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "ombi.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "ombi"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "patrick" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "patrick.veverka.net"
-  value   = "veverkap.github.io"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "patrick"
+  value    = "veverkap.github.io"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "radarr" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "radarr.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "radarr"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "registry" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "registry.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "registry"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "sonarr" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "sonarr.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "sonarr"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "traefik" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "traefik.veverka.net"
-  value   = "lan.veverka.net"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "traefik"
+  value    = "lan.veverka.net"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "warez" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "warez.veverka.net"
-  value   = "f001.backblazeb2.com"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "warez"
+  value    = "f001.backblazeb2.com"
+  type     = "CNAME"
+  ttl      = 120
 }
 resource "cloudflare_record" "webmail" {
-  zone_id = cloudflare_zone.veverka.id
-  name    = "webmail.veverka.net"
-  value   = "ghs.googlehosted.com"
-  type    = "CNAME"
-  ttl     = 120
+  zone_id  = cloudflare_zone.veverka.id
+  name     = "webmail"
+  value    = "ghs.googlehosted.com"
+  type     = "CNAME"
+  ttl      = 120
 }

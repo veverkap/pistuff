7f8f31fa4c8c254e042cd2f7bb7cba183c5ff904
Patrick Veverka <patrick.veverka@gmail.com>
2021-01-19 08:18:39 -0500

    meh

---
+++ roles/consul/terraform/main.tf
@@ -0,0 +1,81 @@
+
+# Configure the Consul provider
+provider "consul" {
+  address    = "192.168.1.231:8500"
+  datacenter = "alpha"
+
+  token      = var.consul_token
+}
+
+resource "consul_node" "linuxplex" {
+  address = "192.168.1.111"
+  name    = "linuxplex"
+}
+
+resource "consul_service" "bazarr" {
+  name    = "bazarr-service"
+  node    = "linuxplex"
+  port    = 6767
+
+  check {
+    check_id                          = "service:bazarr"
+    name                              = "bazarr health check"
+    status                            = "passing"
+    http                              = "192.168.1.111:6767"
+    tls_skip_verify                   = false
+    method                            = "GET"
+    interval                          = "5s"
+    timeout                           = "1s"
+  }
+}
+
+resource "consul_service" "hass" {
+  name    = "hass-service"
+  node    = "rpi6"
+  port    = 8123
+
+  check {
+    check_id                          = "service:hass"
+    name                              = "Hass health check"
+    status                            = "passing"
+    http                              = "192.168.1.152:8123"
+    tls_skip_verify                   = false
+    method                            = "GET"
+    interval                          = "5s"
+    timeout                           = "1s"
+  }
+}
+
+resource "consul_service" "radarr" {
+  name    = "radarr-service"
+  node    = "rpi2"
+  port    = 7878
+
+  check {
+    check_id                          = "service:radarr"
+    name                              = "radarr health check"
+    status                            = "passing"
+    http                              = "192.168.1.224:7878"
+    tls_skip_verify                   = false
+    method                            = "GET"
+    interval                          = "5s"
+    timeout                           = "1s"
+  }
+}
+
+resource "consul_service" "sonarr" {
+  name    = "sonarr-service"
+  node    = "rpi2"
+  port    = 8989
+
+  check {
+    check_id                          = "service:sonarr"
+    name                              = "sonarr health check"
+    status                            = "passing"
+    http                              = "192.168.1.224:8989"
+    tls_skip_verify                   = false
+    method                            = "GET"
+    interval                          = "5s"
+    timeout                           = "1s"
+  }
+}
---
+++ roles/consul/terraform/variables.tf
@@ -0,0 +1,14 @@
+
+variable "consul_address" {
+  description = "Consul Address"
+  default     = "consul.veverka.net"
+}
+
+variable "consul_datacenter" {
+  description = "Consul Datacenter"
+  default     = "alpha"
+}
+
+variable "consul_token" {
+  description = "consul token"
+}
